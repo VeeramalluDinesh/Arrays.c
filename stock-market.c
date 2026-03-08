432571291f1af76d7ee3387368b260dcad467f5b
VeeramalluDinesh <dineshabhiram500@gmail.com>
2026-03-08 21:41:18 +0530

    Add files via upload

---
+++ stock-market.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+int size;
+printf("Enter the size of the array \n");
+scanf("%d",&size);
+int a[size];
+for(int i=0;i<=size-1;i++)
+{
+printf("Enter the %d value :\n",(i+1));
+scanf("%d",&a[i]);
+}
+int buy=0;
+int sell=0;
+int profit=0;
+for(int i=0;i<size-1;i++)
+{
+for(int j=i+1;j<size-1;j++)
+{
+int pur=a[j]-a[i];
+if(pur>profit)
+{buy=a[i];
+sell=a[j];
+profit=pur;}
+}
+}
+printf("buy=%d \t sell=%d\t purchase=%d",buy,sell,profit);
+return 0;
+}
